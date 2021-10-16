13b481f7f73b66c3d1ec96a7654920a97274145d
hoa855182 <hoa855182@gmail.com>
2021-10-16 16:56:43 +0700

    update mid

--- FaceBook/app/src/main/java/usth/facebook/MainActivity.java
+++ FaceBook/app/src/main/java/usth/facebook/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView2 = findViewById(R.id.View_video2);
         String videopath2 ="android.resource://" + getPackageName() + "/" + R.raw.video2;
         Uri uri2 = Uri.parse(videopath2);
-        videoView.setVideoURI(uri2);
+        videoView2.setVideoURI(uri2);
 
         MediaController mediaController2 = new MediaController(this);
         videoView.setMediaController(mediaController2);
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView3 = findViewById(R.id.View_video3);
         String videopath3 ="android.resource://" + getPackageName() + "/" + R.raw.video3;
         Uri uri3 = Uri.parse(videopath3);
-        videoView.setVideoURI(uri3);
+        videoView3.setVideoURI(uri3);
 
         MediaController mediaController3 = new MediaController(this);
         videoView.setMediaController(mediaController3);
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView4 = findViewById(R.id.View_video4);
         String videopath4 ="android.resource://" + getPackageName() + "/" + R.raw.video4;
         Uri uri4 = Uri.parse(videopath4);
-        videoView.setVideoURI(uri4);
+        videoView4.setVideoURI(uri4);
 
         MediaController mediaController4 = new MediaController(this);
         videoView.setMediaController(mediaController4);
